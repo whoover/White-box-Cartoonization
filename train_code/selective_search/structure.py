46b826b4040eb7a226fb8dd74d7666d15b119b9f
Artem Belenkov <whoover@obodev.com>
2021-01-12 23:04:31 +0200

    pretrain fix

--- train_code/selective_search/structure.py
+++ train_code/selective_search/structure.py
@@ -2,7 +2,7 @@ import numpy as np
 from skimage.segmentation import find_boundaries
 from skimage.segmentation import felzenszwalb
 from scipy.ndimage import find_objects
-import measure
+from . import measure
 
 
 class HierarchicalGrouping(object):
